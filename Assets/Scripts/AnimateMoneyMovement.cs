539806c4f34eaa3346fc8d5b027036d843a2e2a0
amani.gh <ghadhabamani94@gmail.com>
2020-06-08 11:30:39 +0200

    Move Money to target[S1]

---
+++ Assets/Scripts/AnimateMoneyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMoneyMovement : MonoBehaviour
+{
+    Vector3 currentPosition;
+    public Transform targetPosition;
+    Rigidbody2D rb;
+    public Vector3 speedDirection = new Vector3(0.3f, 0.3f, 0.3f);
+    private void Awake()
+    {
+        currentPosition = transform.position;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        // move to a specific postion
+        rb.MovePosition(targetPosition.position);
+    }
+}
